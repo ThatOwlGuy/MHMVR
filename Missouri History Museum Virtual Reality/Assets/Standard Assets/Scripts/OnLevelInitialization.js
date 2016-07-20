01d17d6d4023a68add230b795ea9ceb9f96da661
ThatOwlGuy <ssac95@gmail.com>
2016-07-20 17:52:23 -0500

    MHMVR with AR
    UI for a start menu as well as working AR test images with Vuforia.

---
+++ Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/OnLevelInitialization.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+public class OnLevelInitialization extends MonoBehaviour{
+	enum screenRot{portrait, landscape}
+	
+	@SerializeField
+	private var screenMode : screenRot;
+	
+	public function Awake(){
+		
+		switch (screenMode){
+			case screenRot.portrait:
+				Screen.orientation = ScreenOrientation.Portrait;
+				break;
+			case screenRot.landscape:
+				Screen.orientation = ScreenOrientation.LandscapeLeft;
+				break;
+		}
+		
+	}
+}
---
+++ Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/StartMenuButtons.js
@@ -0,0 +1,17 @@
+﻿#pragma strict
+
+public class StartMenuButtons extends MonoBehaviour{
+	
+	@SerializeField
+	private var vrMenu : GameObject;
+	
+	//Enables or disables the VRMenu
+	public function VrMenuEnabled( yes : boolean){
+		vrMenu.SetActive(yes);		
+	}
+	
+	public function LoadLevel(thisLevel : String){
+		//print("Would've loaded " + thisLevel);
+		Application.LoadLevel(thisLevel);
+	}
+}
