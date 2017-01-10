57e28f4cd622627d61400267e355b93c62d34b52
ThatOwlGuy <ssac95@gmail.com>
2017-01-10 07:58:41 -0600

    MHMVR 0.12
    Efficiency  was prioritized and performance has been better optimized.
    Start screen added giving the player the opportunity to either choose to
    go straight into the exhibit or the tutorial

--- Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/Fading.js
+++ Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/Fading.js
@@ -4,8 +4,8 @@ import UnityEngine.UI;
 public class Fading extends MonoBehaviour{
 	public var img : RawImage;
 
-	public var fadeSpeed = 0.8f;
-	private var alpha : float= 1.0f;
+	public var fadeSpeed  : float = 0.8f;
+	private var realFadeSpeed : float;
 
 	public var fading : boolean;
 	public var toWhite : boolean;
@@ -14,12 +14,14 @@ public class Fading extends MonoBehaviour{
 
 	function Awake(){
 		img = gameObject.GetComponent(RawImage);
-		print(img.name);
+		//print(img.name);
 		if(img == null){print("no raw image found");}
 	}
 
 	function LateUpdate(){
 		if(fading){
+			CalculateRealFadeSpeed();
+
 			if(toWhite){
 				AlphaUp();
 			}else{
@@ -44,20 +46,32 @@ public class Fading extends MonoBehaviour{
 	}
 
 	private function AlphaDown(){
-		img.color = Color.Lerp(img.color, Color.clear, fadeSpeed * Time.deltaTime);
-		if(img.color.a < 0.1f){
-			img.color.a = 0.1f;
+		img.color = Color.Lerp(img.color, Color.clear, realFadeSpeed * Time.deltaTime);
+		if(img.color.a < 0.0f){
+			img.color.a = 0.0f;
 			fading = false;
 			faded = true;
 		}
 	}
 
 	private function AlphaUp(){
-		img.color = Color.Lerp(img.color, Color.white, fadeSpeed * Time.deltaTime);
+		img.color = Color.Lerp(img.color, Color.white, realFadeSpeed * Time.deltaTime);
 		if(img.color.a > 0.95f){
 			img.color.a = 1.0f;
 			fading = false;
 			faded = true;
 		}
+	}
+
+	private function CalculateRealFadeSpeed(){
+		var coefficient : float;
+
+		if(toWhite){
+			coefficient = img.color.a + 0.1f;
+		}else{
+			coefficient = Mathf.Abs(img.color.a - 1.0f) + 0.1f;
+		}
+
+		realFadeSpeed = fadeSpeed * fadeSpeed * fadeSpeed * coefficient;
 	}
 }
--- Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/PortalManagement/LoadLevelOnTrigger.js
+++ Missouri History Museum Virtual Reality/Assets/Standard Assets/Scripts/PortalManagement/LoadLevelOnTrigger.js
@@ -5,16 +5,27 @@ public var levelToLoad : String;
 public var loading : boolean;
 
 private var darthFader : Fading;
+private var otherLevelLoaders : GameObject[];
 
 function Awake(){
 	darthFader = FindObjectOfType(Fading);
+	otherLevelLoaders = GameObject.FindGameObjectsWithTag("Triggerable");
 }
 
 function OnTriggerEnter(other : Collider){
 	if(other.tag == "Player"){
-		darthFader.FadeToWhite();
-		loading = true;
+		Triggered();
+	}
+}
+
+function Triggered(){
+	darthFader.FadeToWhite();
+	loading = true;
+	if(FindObjectOfType(PlayerController))
 		FindObjectOfType(PlayerController).speed = 0;
+
+	for(var i : int = 0; i < otherLevelLoaders.length; i++){
+		otherLevelLoaders[i].tag = "Untagged";
 	}
 }
 
